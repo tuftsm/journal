fd4aad9a4ef7987964ec3f1db4ef6881402892f5
tuftsm <student.mtolo@gmail.com>
2022-11-26 19:18:40 +0000

    added multiple pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,96 +1,25 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Workouts from "./pages/Workouts";
+import Home from "./pages/Home";
+import Personal from "./pages/Personal";
 
-function App() {
-  // setup state
-  const [entries, setEntries] = useState([]);
-  const [error, setError] = useState("");
-  const [date, setDate] = useState("");
-  const [info, setInfo] = useState("");
-  const [highlight, setHighlight] = useState("");
-
-  const fetchEntries = async() => {
-    try {      
-      const response = await axios.get("/api/entries");
-      setEntries(response.data.entries);
-    } catch(error) {
-      setError("error retrieving entries: " + error);
-    }
-  }
-  const createEntry = async() => {
-    try {
-      await axios.post("/api/entries", {date: date, info: info, highlight: highlight});
-    } catch(error) {
-      setError("error adding an entry: " + error);
-    }
-  }
-  const deleteOneEntry = async(entry) => {
-    try {
-      await axios.delete("/api/entries/" + entry.id);
-    } catch(error) {
-      setError("error deleting an entry" + error);
-    }
-  }
-
-  // fetch ticket data
-  useEffect(() => {
-    fetchEntries();
-  },[]);
-
-  const addEntry = async(e) => {
-    e.preventDefault();
-    await createEntry();
-    fetchEntries();
-    setDate("");
-    setInfo("");
-    setHighlight("");
-  }
-
-  const deleteEntry = async(entry) => {
-    await deleteOneEntry(entry);
-    fetchEntries();
-  }
-
-  // render results
+export default function App() {
   return (
-    <div className="App">
-      {error}
-      <h1>Create an Entry</h1>
-      <form onSubmit={addEntry}>
-        <div>
-          <label>
-            Date:
-            <input type="text" value={date} onChange={e => setDate(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Info:
-            <textarea value={info} onChange={e=>setInfo(e.target.value)}></textarea>
-          </label>
-        </div>
-        <div>
-          <label>
-            Highlight:
-            <textarea value={highlight} onChange={e=>setHighlight(e.target.value)}></textarea>
-          </label>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-      <h1>Past Info</h1>
-      {entries.map( entry => (
-        <div key={entry.id} className="entry">
-          <div className="info">
-            <p>{entry.info}</p>
-            <p>{entry.highlight}</p>
-            <p><i>-- {entry.date}</i></p>
-          </div>
-          <button onClick={e => deleteEntry(entry)}>Delete</button>
-        </div>
-      ))}     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Personal" element={<Personal />} />
+          <Route path="Workouts" element={<Workouts />} />
+          <Route path="*" element={<Navigate to="/" />}  />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+
